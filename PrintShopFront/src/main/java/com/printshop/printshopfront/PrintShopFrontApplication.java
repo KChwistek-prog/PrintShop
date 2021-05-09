b010482c1d4449c4b6d6bef44562ee64a2aa2e28
K.Chwistek <k.chwistek@outlook.com>
2021-05-09 13:17:59 +0200

    new module for vue js front

---
+++ PrintShopFront/src/main/java/com/printshop/printshopfront/PrintShopFrontApplication.java
@@ -0,0 +1,13 @@
+package com.printshop.printshopfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintShopFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintShopFrontApplication.class, args);
+    }
+
+}
