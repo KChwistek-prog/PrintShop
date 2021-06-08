a08d9467421855a33b7f75718f020664484bdb23
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2021-06-08 11:03:51 +0200

    employee repo spock tests

--- src/test/groovy/com/printshopmanagement/application/repository/EmployeeRepositoryTestSuite.groovy
+++ src/test/groovy/com/printshopmanagement/application/repository/EmployeeRepositoryTestSuite.groovy
@@ -3,24 +3,55 @@ package com.printshopmanagement.application.repository
 import com.printshopmanagement.application.domain.Employee
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
 
 @SpringBootTest
-class EmployeeRepositoryTestSuite extends Specification{
+class EmployeeRepositoryTestSuite extends Specification {
 
     @Autowired
     EmployeeDbService employeeDbService
 
-    Employee employee
-
-    def setup(){
-        employee = new Employee(1L, 231541521L, "John", "Doe", "Elm Str", "Working", 2500L)
+    def "should return list of employees"() {
+        given:
+            def john = new Employee(231541521L, "John", "Doe", "Elm Str", "Working", 2500L)
+            def ann = new Employee(231541521L, "Ann", "Doe", "Elm Str", "Working", 2500L)
+            employeeDbService.saveEmployee(john)
+            employeeDbService.saveEmployee(ann)
+        when:
+            var result = employeeDbService.getAllEmployees()
+        then:
+            result.size() == 2
     }
 
-    def "database service should save employee"(){
+    def "database service should return John"() {
+        setup:
+        def john = new Employee(231541521L, "John", "Doe", "Elm Str", "Working", 2500L)
+        employeeDbService.saveEmployee(john)
+
         when:
-        employeeDbService.saveEmployee(employee)
+        var result = employeeDbService.getEmployee(1L)
         then:
-        employeeDbService.getEmployee(1L).isPresent()
+        result.orElseThrow().getEmployeeFirstName() == "John"
+    }
+
+    def "database service should save employee"() {
+        given:
+            def john = new Employee(231541521L, "John", "Doe", "Elm Str", "Working", 2500L)
+        when:
+            employeeDbService.saveEmployee(john)
+        then:
+            employeeDbService.getEmployee(1L).isPresent()
+            employeeDbService.getEmployee(1L).get().employeeFirstName == "John"
+    }
+
+    def "database service should remove employee from db"() {
+        given:
+            def john = new Employee(231541521L, "John", "Doe", "Elm Str", "Working", 2500L)
+            employeeDbService.saveEmployee(john)
+        when:
+            employeeDbService.deleteEmployee(1L)
+        then:
+            employeeDbService.getEmployee(1L).isEmpty()
     }
 }
